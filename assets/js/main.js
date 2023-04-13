74aa29b6ed1878070480f7f3cd360b5d482d5cb1
SteAnoe <ste.anoe@gmail.com>
2023-04-13 14:17:59 +0200

    js - creazione tramite array object + next - prev arrows

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,53 +8,108 @@
 // Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
 // MILESTONE 3
 // Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+// BONUS 2:
+// Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
+// BONUS 3:
+// Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
 
-let arrayImg = [
-    'https://picsum.photos/id/235/800/400',
-    'https://picsum.photos/id/236/800/400',
-    'https://picsum.photos/id/237/800/400',
-    'https://picsum.photos/id/238/800/400',
-    'https://picsum.photos/id/239/800/400'
-]
+const arrayImg = [
+    {
+        image: 'https://picsum.photos/id/235/800/400',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'https://picsum.photos/id/236/800/400',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'https://picsum.photos/id/237/800/400',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'https://picsum.photos/id/238/800/400',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'https://picsum.photos/id/239/800/400',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
 
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
 
-let currentImg = 0;
-let img = document.querySelector('.img');
-img.src = "https://picsum.photos/id/235/800/400"
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+console.log(prev, next);
+let activeImg = 0;
+const imgMain = document.querySelector('.slider')
+const thumbMain = document.querySelector('.thumbnails')
+
+for (let i = 0; i < arrayImg.length; i++) {
+    let currentImg = arrayImg[i]
+
+    imgMain.innerHTML += `  
+        <div id=img${i} class="d-none">
+            <img src="${currentImg.image}" class="img" alt="..."> 
+            <div class="testo">
+                <h5>${currentImg.title}</h5>
+                <p>${currentImg.text}</p>
+            </div>    
+        </div> 
+    `
+
+    thumbMain.innerHTML += `  
+        <div class="thumbnail">
+            <img id=thumb${i} src="${currentImg.image}" class="" alt="...">
+        </div>
+    `
+
+
+    
+
+}
+
+imgMain.querySelectorAll('.d-none')[activeImg].classList.add('active')
 
 next.addEventListener('click', function(){
-    if( currentImg == arrayImg.length - 1) {
-        currentImg = 0;
+    if( activeImg == arrayImg.length - 1) {
+        activeImg = 0;
     } else {
-        currentImg++;
+        activeImg++;
     }
-    img.src = arrayImg[currentImg];
+
+    document.querySelector('.d-none.active').classList.remove('active')
+    imgMain.querySelectorAll('.d-none')[activeImg].classList.add('active')
 })
 
 prev.addEventListener('click', function(){
-    if( currentImg == 0 ){
-        currentImg = arrayImg.length - 1;
+    if( activeImg == 0 ){
+        activeImg = arrayImg.length - 1;
     } else {
-    currentImg--;
+        activeImg--;
     }
-    img.src = arrayImg[currentImg];
+
+    document.querySelector('.d-none.active').classList.remove('active')
+    imgMain.querySelectorAll('.d-none')[activeImg].classList.add('active')
 })
 
-let thumbnails = document.querySelector('.thumbnails')  
 
-for (let i = 0; i < arrayImg.length; i++) {
-    let div = document.createElement('div');
-    thumbnails.appendChild(div);
-    div.classList.add('thumbnail');
-    div.setAttribute('onclick', `changeImg(${i})`)
-    let imgThumbnail = document.createElement('img');
-    imgThumbnail.setAttribute('src', arrayImg[i]);
-    div.append(imgThumbnail);
-}
+
+
+
+// let thumbnails = document.querySelector('.thumbnails')  
+
+// for (let i = 0; i < arrayImg.length; i++) {
+//     let div = document.createElement('div');
+//     thumbnails.appendChild(div);
+//     div.classList.add('thumbnail');
+//     div.setAttribute('onclick', `changeImg(${i})`)
+//     let imgThumbnail = document.createElement('img');
+//     imgThumbnail.setAttribute('src', arrayImg[i]);
+//     div.append(imgThumbnail);
+// }
  
-function changeImg(num){
-    img.src = arrayImg[num];
-    currentImg = num;
-}
+// function changeImg(num){
+//     img.src = arrayImg[num];
+//     currentImg = num;
+// }
